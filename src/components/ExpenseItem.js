5804a650a1a2ec52e609a28d8d21206287f9321d
Saidul Islam <saidulislamjihad20@gmail.com>
2022-12-21 05:12:51 -0800

    First commit

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,11 @@
+import './ExpenseItem.css'
+
+function ExpenseItem(){
+    return(
+        <h1 className="text-3xl italic underline hover:not-italic">
+            Hello dsadas!
+        </h1>
+    )
+}
+
+export default ExpenseItem;
